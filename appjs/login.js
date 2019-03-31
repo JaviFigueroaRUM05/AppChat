51b12e9f2a62ce7a687c47976b75bcc26ae75e9d
TheParodicts <brianrodrig@gmail.com>
2019-03-31 00:30:23 -0400

    Implemented preliminary login

--- appjs/login.js
+++ appjs/login.js
@@ -1,21 +1,37 @@
-angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scope',
-    function($http, $log, $scope) {
+angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scope', '$window',
+    function($http, $log, $scope, $window) {
         var thisCtrl = this;
+        this.Error = "";
+        this.uid= null;
 
-       this.formSubmit = function(){
-        if(this.login()){
-                $scope.error='';
-                $scope.username='';
-                $scope.password='';
-                $http('/chat');
-            } else{
-                $scope.error = 'Incorrect username/password!';
-                }
-       };
+
+
+
+
+//TODO Look into catch, why if we get the error it wont go through the thens.
 
        this.login = function(){
-       console.log($scope.username + $scope.password);
-       return true;
+        $http({
+          method: 'POST',
+          url: 'http://127.0.0.1:5000/user/login',
+          data: JSON.stringify({ "email": $scope.username,
+                                 "password": $scope.password})
+        }).then(
+            function(response){
+                var response_json = response.data
+                if (response_json.hasOwnProperty('Error')){
+                    thisCtrl.Error = response_json.Error;}
+
+                else{thisCtrl.uid = response_json.uid;}
+        }).then (
+            function(){
+                if(thisCtrl.uid != null){
+                    $scope.error='';
+                    $scope.username='';
+                    $scope.password='';
+                    $window.location.href = '/AppChat/index.html#!/chat';
+                } else{$scope.error = 'Incorrect username/password!';
+        }}).catch(angular.noop);
        }
 
 }]);
