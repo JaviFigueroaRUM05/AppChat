3e17a0254889e89cbd4570c9e0b9d87d6fe6a1d2
TheParodicts <brianrodrig@gmail.com>
2019-03-31 23:10:49 -0400

    Show user's contacts in user nav bar

--- appjs/userNav.js
+++ appjs/userNav.js
@@ -8,6 +8,8 @@ angular.module('AppChat').controller('UserNavController', ['$http', '$log', '$sc
         this.email='';
         this.phone='';
 
+        this.contactList = [];
+
 //       $scope.toggleUserNav = buildToggler('right');
 
 //    this.buildToggler = function(componentId) {
@@ -38,6 +40,24 @@ angular.module('AppChat').controller('UserNavController', ['$http', '$log', '$sc
           )
        };
 
+        this.getUserContacts = function(){
+            $http({
+            method: 'GET',
+            url: 'http://127.0.0.1:5000/user/contacts',
+            headers: { "Authorization": $cookies.get('uid')}
+          }).then(
+                function(success_response){
+                var contacts = success_response.data.contacts;
+                for(contact in contacts){
+                    thisCtrl.contactList.push(contacts[contact]);
+                    }
+                console.log(thisCtrl.contactList);
+                }, function(error_response){
+                console.log(error_response);}
+          )
+       };
+
 
     this.getUserInfo();
+    this.getUserContacts();
 }]);
