c151b4c4aadecaf840e5496867bc87f97a3374d2
sosv-hydro <sofia.saavedra1@upr.edu>
2019-04-07 13:20:13 -0400

    Merged Brian Stuff in
    Got a semi functional chat page with login
    
    TODO: STYLE

---
+++ appjs/login.js
@@ -0,0 +1,36 @@
+angular.module('AppChat').controller('LoginController', ['$http', '$log', '$scope', '$window', '$cookies',
+    function($http, $log, $scope, $window, $cookies) {
+        var thisCtrl = this;
+        this.Error = "";
+        this.uid= null;
+
+       this.login = function(){
+        $http({
+          method: 'POST',
+          url: 'http://127.0.0.1:5000/user/login',
+          data: JSON.stringify({ "email": $scope.username,
+                                 "password": $scope.password})
+
+        }).then( // On success.
+            function(success_response){
+                thisCtrl.uid = success_response.data.uid;
+
+        }, function (error_response){ //On Error
+                console.log(error_response);
+                if (error_response.data.hasOwnProperty('Error')){
+                    thisCtrl.Error = error_response.data.Error;
+                    console.log(thisCtrl.Error);}
+                else{
+                    console.log(error_response);}
+        }).then (
+            function(){
+                if(thisCtrl.uid != null){
+                    $scope.error='';
+                    $scope.username='';
+                    $scope.password='';
+                    $cookies.put('uid', thisCtrl.uid);
+                    //console.log($cookies.get('uid'));
+                    $window.location.href = '/#!/chat';
+        }}).catch(angular.noop);
+       };
+}]);
---
+++ appjs/userNav.js
@@ -0,0 +1,63 @@
+angular.module('AppChat').controller('UserNavController', ['$http', '$log', '$scope', '$window', "$cookies",
+    function($http, $log, $scope, $window, $cookies) {
+        var thisCtrl = this;
+//
+//
+//        this.fullName = "";
+//        this.username = "";
+//        this.email='';
+//        this.phone='';
+
+        this.contactList = [];
+
+//       $scope.toggleUserNav = buildToggler('right');
+
+//    this.buildToggler = function(componentId) {
+//      return function() {
+//        $mdSidenav(componentId).toggle();
+//      };
+
+
+
+     /* this.toggleUserNavBar = function(){
+            thisCtrl.userNavBarToggled = !thisCtrl.userNavBarToggled;
+            console.log(thisCtrl.userNavBarToggled);
+          //  groupCtrl.showGroupInfo(group.gName, group.GID)
+          };*/
+
+//       this.getUserInfo = function(){
+//            $http({
+//            method: 'GET',
+//            url: 'http://127.0.0.1:5000/user/uid='+ $cookies.get('uid')
+//          }).then(
+//                function(success_response){
+//                var response_data = success_response.data;
+//                thisCtrl.fullName = response_data.first_name + " " + response_data.last_name;
+//                thisCtrl.username = response_data.uname;
+//                thisCtrl.email = response_data.email;
+//                thisCtrl.phone = response_data.phone;
+//                }
+//          )
+//       };
+
+        this.getUserContacts = function(){
+            $http({
+            method: 'GET',
+            url: 'http://127.0.0.1:5000/user/contacts',
+            headers: { "Authorization": $cookies.get('uid')}
+          }).then(
+                function(success_response){
+                var contacts = success_response.data.contacts;
+                for(contact in contacts){
+                    thisCtrl.contactList.push(contacts[contact]);
+                    }
+                console.log(thisCtrl.contactList);
+                }, function(error_response){
+                console.log(error_response);}
+          )
+       };
+
+
+//    this.getUserInfo();
+    this.getUserContacts();
+}]);
