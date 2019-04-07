000e1fdf42558362574199862d3361630d7a5fab
TheParodicts <brianrodrig@gmail.com>
2019-04-07 01:06:38 -0400

    Merged Sofia's code with Brian's code, and cleaned up some comments.

--- appjs/userNav.js
+++ appjs/userNav.js
@@ -1,44 +1,7 @@
 angular.module('AppChat').controller('UserNavController', ['$http', '$log', '$scope', '$window', "$cookies",
     function($http, $log, $scope, $window, $cookies) {
         var thisCtrl = this;
-//
-//
-//        this.fullName = "";
-//        this.username = "";
-//        this.email='';
-//        this.phone='';
-
         this.contactList = [];
-
-//       $scope.toggleUserNav = buildToggler('right');
-
-//    this.buildToggler = function(componentId) {
-//      return function() {
-//        $mdSidenav(componentId).toggle();
-//      };
-
-
-
-     /* this.toggleUserNavBar = function(){
-            thisCtrl.userNavBarToggled = !thisCtrl.userNavBarToggled;
-            console.log(thisCtrl.userNavBarToggled);
-          //  groupCtrl.showGroupInfo(group.gName, group.GID)
-          };*/
-
-//       this.getUserInfo = function(){
-//            $http({
-//            method: 'GET',
-//            url: 'http://127.0.0.1:5000/user/uid='+ $cookies.get('uid')
-//          }).then(
-//                function(success_response){
-//                var response_data = success_response.data;
-//                thisCtrl.fullName = response_data.first_name + " " + response_data.last_name;
-//                thisCtrl.username = response_data.uname;
-//                thisCtrl.email = response_data.email;
-//                thisCtrl.phone = response_data.phone;
-//                }
-//          )
-//       };
 
         this.getUserContacts = function(){
             $http({
@@ -57,7 +20,5 @@ angular.module('AppChat').controller('UserNavController', ['$http', '$log', '$sc
           )
        };
 
-
-//    this.getUserInfo();
     this.getUserContacts();
 }]);
